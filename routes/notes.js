1a9b2ee429fb130dcad11d26b7e73802566540b5
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-10-05 15:47:09 -0600

    get and post request running in insomnia

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,7 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
+const fs = require('fs')
 
 // GET Route for retrieving all the notes
 notes.get('/notes', (req, res) => {
@@ -19,8 +20,22 @@ notes.post('/notes', (req, res) => {
       text,
       note_id: uuid(),
     };
+    fs.readFile('./db/notes.json', 'utf8', function (err, data){
+      var jsondata = JSON.parse(data)
 
-    readAndAppend(newNote, './db/notes.json');
+      jsondata.push(req.body)
+     fs.writeFile('./db/notes.json', JSON.stringify(jsondata), function(err){if (err) throw err}
+     )
+    })
+    
+    //fs.writeFile(newNote, JSON.stringify(newNote))
+    
+    // readAndAppend('./db/notes.json', function (err, newNote) {
+    //   const json = JSON.parse(req.body)
+    //   json.push('newNote' + newNote)
+
+      //fs.writeFile("results.json", JSON.stringify(json))
+    // });
     res.json(`Note added successfully 🚀`);
   } else {
     res.error('Error in adding note');
