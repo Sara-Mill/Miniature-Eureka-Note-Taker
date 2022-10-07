9ad3f5bade00c4e8b20343bb87f2dadec0b10c01
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-10-06 23:05:16 -0600

    cleaned up file structure

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,5 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-const uuid = require('../helpers/uuid');
-const fs = require('fs')
-const noteshtml = require('../public/index')
+const fs = require('fs');
 const db = require('../db/notes.json');
 
 let id = db.length + 1;
@@ -14,10 +11,8 @@ let id = db.length + 1;
 
       // POST Route for adding a new note
       notes.post('/notes', (req, res) => {
-        console.log(req.body);
 
         req.body.id = id++;
-        console.log(req.body);
 
         db.push(req.body);
         fs.writeFile('./db/notes.json', JSON.stringify(db), function (err) {
@@ -34,7 +29,7 @@ let id = db.length + 1;
           if (db[i].id === id) {
             db.splice(i, 1);
           }
-        }
+        };
         console.log(db);
 
         fs.writeFile('./db/notes.json', JSON.stringify(db), function (err) {
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//dependencies
+//Dependencies
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -11,7 +11,7 @@ const app = express();
 
 const PORT = 3001;
 
-//middleware
+//Middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
@@ -19,18 +19,18 @@ app.use(express.json());
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-//get route for html homepage
+//Get route for html homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './publicindex.html'))
 });
 
-//get route for notes html page
+//Get route for notes html page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 
-//sets local port for listening
+//Sets local port for listening
 app.listen(PORT, () =>
 console.log(`App listening at http://localhost:${PORT}`)
 );
