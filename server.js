8f8e4ed192156b6653aad8019012520ad2280b6a
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-10-09 08:32:32 -0600

    changed PORt to include process.env

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const apiRoutes = require('./routes/apiRoutes');
 
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3002;
 
 //Middleware
 app.use(express.static('public'));
